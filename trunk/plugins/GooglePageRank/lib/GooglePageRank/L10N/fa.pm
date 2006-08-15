aecc24a9b8ed2f1664d24dcf1ef0c2b41ce16ac5
nekofar <nekofar@f660550f-071b-0410-899b-d33b69d59c05>
2006-08-15 21:07:26 +0000

    New Import

---
+++ trunk/plugins/GooglePageRank/lib/GooglePageRank/L10N/fa.pm
@@ -0,0 +1,22 @@
+# GooglePageRank plugin for Movable Type
+# Author: Milad Nekofar (http://www.nekofar.com)
+# Released under the Artistic License
+#
+# $Id$
+
+package GooglePageRank::L10N::fa;
+
+use strict;
+use base 'GooglePageRank::L10N::en_us';
+use vars qw( %Lexicon );
+
+## The following is the translation table.
+
+%Lexicon = (
+    'Google PageRank' => 'پیج رنک گوگل',
+    'This plugin allow you add the Google PageRank of the current blog into your pages.' => 'این پلاگین امکان افزودن پیج رنک گوگل وبلاگ جاری را در صفحات شما، فراهم می کند.',
+    'Milad Nekofar' => 'میلاد نکوفر',   
+);
+
+1;
+
